7f9687f0c06ef111b27e94e2c3e08d89b8ab71cf
momo1108 <momo1108@daum.net>
2024-06-25 10:18:24 +0900

    chore: Markdown 린팅 ignore 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,26 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignores: ['**/*.md'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-        'no-var': 'error',
-        eqeqeq: 'error',
-        'dot-notation': 'error',
-        'react/jsx-pascal-case': 'error',
-        'react/react-in-jsx-scope': 'off',
-    },
+    'no-var': 'error',
+    eqeqeq: 'error',
+    'dot-notation': 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
