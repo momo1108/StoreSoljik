e8f7ad30622f49f3da2926f5f8325d9a3ae8d75c
momo1108 <momo1108@daum.net>
2024-07-06 19:52:27 +0900

    feat: items 페이지 구현 완료

--- src/pages/seller/items/Items.Style.tsx
+++ src/pages/seller/items/Items.Style.tsx
@@ -1,2 +1,59 @@
 // import media from '@/style/media';
-// import styled from 'styled-components';
+import { H2 } from '@/components/ui/header/Header.Style';
+import styled from 'styled-components';
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1140px;
+  padding: 0 10px;
+  gap: 25px;
+`;
+
+export const ItemsTitleHeader = styled(H2)`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 0 10px;
+`;
+
+export const ItemsListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+export const ItemsListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ItemsListContentBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 15px 0;
+`;
+
+export const ErrorBox = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  color: ${(props) => props.theme.color.destructive};
+  font-size: ${(props) => props.theme.fontSize.xl};
+  font-weight: bold;
+`;
+
+export const SpinnerBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 150px;
+  padding: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InViewDiv = styled.div`
+  width: 100%;
+`;
--- src/pages/seller/items/Items.tsx
+++ src/pages/seller/items/Items.tsx
@@ -1,12 +1,86 @@
 import Header from '@/components/layouts/header/Header';
 import Main from '@/components/layouts/main/Main';
-// import * as S from './Items.Style';
+import { H3, H4 } from '@/components/ui/header/Header.Style';
+import * as S from './Items.Style';
+import { RiListCheck } from 'react-icons/ri';
+import HorizontalCard from '@/components/ui/card/horizontalcard/HorizontalCard';
+import useItems from './useItems';
+import Spinner from '@/components/ui/spinner/Spinner';
+import Button from '@/components/ui/button/Button';
 
 const Items: React.FC = () => {
+  const {
+    onClickRegistration,
+    data,
+    error,
+    status,
+    isFetchingNextPage,
+    isLoading,
+    ref,
+    navigateToUpdate,
+    deleteItem,
+  } = useItems();
   return (
     <>
       <Header userType={'seller'}></Header>
-      <Main>hello</Main>
+      <Main>
+        <S.ItemsContainer>
+          <S.ItemsTitleHeader>
+            <RiListCheck />
+            판매 상품 관리
+          </S.ItemsTitleHeader>
+          <H3>판매 상품 상세 목록</H3>
+          <S.ItemsListContainer>
+            <S.ItemsListHeader>
+              <H4>등록된 상품들</H4>
+              <Button styleType='primary' onClick={onClickRegistration}>
+                판매 상품 등록
+              </Button>
+            </S.ItemsListHeader>
+            <S.ItemsListContentBox>
+              {status === 'pending' ? (
+                <S.SpinnerBox>
+                  <Spinner spinnerSize={32}>
+                    판매 상품을 불러오는 중입니다.
+                  </Spinner>
+                </S.SpinnerBox>
+              ) : status === 'error' ? (
+                <S.ErrorBox>
+                  {error?.message || '판매 상품을 불러오지 못했습니다.'}
+                </S.ErrorBox>
+              ) : (
+                data?.pages.map((page) => {
+                  return page.productArray.slice(0, 10).map((item) => {
+                    return (
+                      <HorizontalCard
+                        data={item}
+                        key={item.id}
+                        handleClickUpdate={() => navigateToUpdate(item)}
+                        handleClickDelete={() => {
+                          if (
+                            confirm(
+                              `"${item.productName}" 상품을 삭제하시겠습니까?`,
+                            )
+                          )
+                            deleteItem.mutate(item.id);
+                        }}
+                      />
+                    );
+                  });
+                })
+              )}
+              {!isLoading && isFetchingNextPage && (
+                <S.SpinnerBox>
+                  <Spinner spinnerSize={32}>
+                    다음 판매 상품들을 불러오는 중입니다.
+                  </Spinner>
+                </S.SpinnerBox>
+              )}
+              <S.InViewDiv ref={ref}></S.InViewDiv>
+            </S.ItemsListContentBox>
+          </S.ItemsListContainer>
+        </S.ItemsContainer>
+      </Main>
     </>
   );
 };
