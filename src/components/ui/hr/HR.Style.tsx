7cd37035c1235f3b0aba821180619c7cd7073917
momo1108 <momo1108@daum.net>
2024-07-09 22:30:49 +0900

    feat: HR 스타일 태그 생성

---
+++ src/components/ui/hr/HR.Style.tsx
@@ -0,0 +1,11 @@
+import media from '@/style/media';
+import styled from 'styled-components';
+
+export const StyledHR = styled.hr`
+  margin: 0;
+  padding: 0;
+  border: none;
+  width: 100%;
+  height: 1.5px;
+  background: #898f97;
+`;
