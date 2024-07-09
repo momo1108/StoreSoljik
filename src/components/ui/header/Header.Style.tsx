fbc3be7e4296cbe394d2a57400d09d1f45baa68d
momo1108 <momo1108@daum.net>
2024-07-09 20:40:13 +0900

    modify: Header 컴포넌트에 line height 추가

--- src/components/ui/header/Header.Style.tsx
+++ src/components/ui/header/Header.Style.tsx
@@ -2,13 +2,17 @@ import styled from 'styled-components';
 
 export const H1 = styled.h1`
   font-size: ${(props) => props.theme.fontSize.xxxl};
+  line-height: ${(props) => props.theme.lineHeight.xxxl};
 `;
 export const H2 = styled.h2`
   font-size: ${(props) => props.theme.fontSize.xxl};
+  line-height: ${(props) => props.theme.lineHeight.xxl};
 `;
 export const H3 = styled.h3`
   font-size: ${(props) => props.theme.fontSize.xl};
+  line-height: ${(props) => props.theme.lineHeight.xl};
 `;
 export const H4 = styled.h4`
   font-size: ${(props) => props.theme.fontSize.lg};
+  line-height: ${(props) => props.theme.lineHeight.lg};
 `;
