ab30fd89c92ffc337a22157f7f4591d03b599a85
momo1108 <momo1108@daum.net>
2024-07-09 20:37:06 +0900

    modify: 디렉터리명 productcard 로 변경 및 카드 컴포넌트 완성

---
+++ src/components/ui/productcard/verticalcard/VerticalCard.Style.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import Button from '../../button/Button';
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 220px;
+`;
+
+export const CardImageBox = styled.div<{ $src: string }>`
+  width: 100%;
+  padding-top: 75%;
+  background-image: url(${(props) => props.$src});
+  background-size: cover;
+  background-position: center;
+`;
+
+export const CardContentBox = styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border-top: 1px solid ${(props) => props.theme.color.brighterGray};
+`;
+
+export const CardContentTitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const DatetimeP = styled.p`
+  font-size: ${(props) => props.theme.fontSize.xs};
+  color: #666;
+`;
+
+export const DescriptionP = styled.p`
+  font-size: ${(props) => props.theme.fontSize.sm};
+  color: #333;
+  height: 40.5px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+`;
+
+export const PriceQuantityBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+`;
+
+export const PriceP = styled.p`
+  display: flex;
+  flex-direction: column;
+  color: #333;
+  font-weight: bold;
+  #title {
+    font-size: ${(props) => props.theme.fontSize.sm};
+    margin-right: 8px;
+  }
+  #price {
+    color: #e60023;
+  }
+`;
+
+export const MoreButton = styled(Button)`
+  padding: 5px;
+`;
