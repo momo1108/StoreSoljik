d62fecaa5caffb4ec1cedd92fccae5c2b92d959c
momo1108 <momo1108@daum.net>
2024-07-12 02:22:15 +0900

    modify: 배열 비교 함수, 딜레이 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,21 @@ export const getIsoDate: (isoString: string) => string = (isoString) =>
 
 export const getIsoTime: (isoString: string) => string = (isoString) =>
   isoString.slice(11, 16);
+
+export function compareArray<T>(array1: Array<T>, array2: Array<T>): boolean {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (
+        typeof array1[i] === typeof array2[i] &&
+        typeof array1[i] === 'object'
+      )
+        return compareArray(array1[i] as Array<T>, array2[i] as Array<T>);
+      else if (typeof array1[i] !== typeof array2[i]) return false;
+      else if (array1[i] !== array2[i]) return false;
+    }
+    return true;
+  } else return false;
+}
+
+export const sleep: (delay: number) => Promise<void> = (delay) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
