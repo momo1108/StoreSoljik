f69221d30a7885564e904d905bd00aee62493ab7
momo1108 <momo1108@daum.net>
2024-10-24 19:57:26 +0900

    feat: orderStatus 상태 추가

--- src/pages/seller/management/useManagement.ts
+++ src/pages/seller/management/useManagement.ts
@@ -1,10 +1,15 @@
 import { useFirebaseAuth } from '@/hooks/useFirebaseAuth';
 import { fetchOrders } from '@/services/orderService';
+import { OrderStatus } from '@/types/FirebaseType';
 import { useQuery } from '@tanstack/react-query';
 import { orderBy, where } from 'firebase/firestore';
+import { useState } from 'react';
 
 const useManagement = () => {
   const { userInfo } = useFirebaseAuth();
+  const [selectedOrderStatus, setSelectedOrderStatus] = useState<OrderStatus>(
+    OrderStatus.All,
+  );
 
   /**
    * 화면 상단의 주문 현황을 위한 코드
@@ -12,8 +17,9 @@ const useManagement = () => {
   const { data: timeOrderData, status: timeOrderStatus } = useQuery({
     queryKey: ['orders'],
     queryFn: async () => {
+      const filters = [where('sellerId', '==', userInfo!.uid)];
       return await fetchOrders({
-        filters: [where('sellerId', '==', userInfo!.uid)],
+        filters,
         sortOrders: [orderBy('createdAt', 'desc')],
       });
     },
