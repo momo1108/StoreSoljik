06cfcd3d84ecc8b7316f0a0cc8b36946a077d3e5
momo1108 <momo1108@daum.net>
2024-10-25 21:04:56 +0900

    modify: 타이틀 컴포넌트 수정

--- src/pages/seller/management/Management.Style.tsx
+++ src/pages/seller/management/Management.Style.tsx
@@ -1,3 +1,4 @@
+import { H2 } from '@/components/ui/header/Header.Style';
 import styled from 'styled-components';
 
 export const ManagementContainer = styled.div`
@@ -5,4 +6,17 @@ export const ManagementContainer = styled.div`
   flex-direction: column;
   width: 800px;
   gap: 32px;
+`;
+
+export const ManagementTitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const ManagementTitleHeader = styled(H2)`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 0 10px;
 `;
--- src/pages/seller/management/Management.tsx
+++ src/pages/seller/management/Management.tsx
@@ -2,17 +2,24 @@ import Header from '@/components/layouts/header/Header';
 import Main from '@/components/layouts/main/Main';
 import useManagement from './useManagement';
 import * as S from './Management.Style';
-import { H2 } from '@/components/ui/header/Header.Style';
+import { IoMdSettings } from 'react-icons/io';
 
 const Management: React.FC = () => {
   const {} = useManagement();
 
+  // https://dribbble.com/search/information-table
   return (
     <>
       <Header userType={'seller'}></Header>
       <Main>
         <S.ManagementContainer>
-          <H2>주문 상태 관리</H2>
+          <S.ManagementTitleBox>
+            <S.ManagementTitleHeader>
+              <IoMdSettings />
+              주문 상태 관리
+            </S.ManagementTitleHeader>
+            <p>판매 등록된 상품들의 주문을 확인하고 상태를 갱신해주세요.</p>
+          </S.ManagementTitleBox>
         </S.ManagementContainer>
       </Main>
     </>
