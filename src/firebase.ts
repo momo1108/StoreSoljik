1690c0a0d1ecfea3164aa25f2a4ca10019981aed
momo1108 <momo1108@daum.net>
2025-02-04 21:27:12 +0900

    chore: 리팩토링 관련 설정 및 개발 의존성 추가 - 리팩토링 스크립트를 실행하기 위한 의존성 추가   - tsx : Node.js 환경에서 리팩토링 타입스크립트 실행   - tsconfig-paths : 타입스크립트 설정파일의 path 설정을 Node.js 환경에서도 적용   - dotenv : firebase 모듈을 사용 시 인증 관련 환경변수를 Vite 환경 뿐 아니라 Node.js 환경에서도 사용하기 위한 모듈 - 리팩토링 스크립트 내에서 모듈 경로를 인식하기 위해 tsconfig.json 에 compilerOptions 를 추가.(references 내부의 경로 설정만으으로는 해결이 안되는 현상을 발견)

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,28 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// Node.js 환경일 경우 dotenv 로드
+if (typeof process !== 'undefined' && process.env) {
+  const dotenv = await import('dotenv'); // ES Module에서 동적 import
+  dotenv.config();
+}
+
+// 실행 환경(Vite 로 실행되는 웹앱 or 리팩토링 스크립트만 실행하는 Node.js 환경)에 따라 환경 변수 가져오기
+const getEnv = (key: string) => {
+  if (typeof import.meta !== 'undefined' && import.meta.env) {
+    return import.meta.env[key];
+  }
+  return process.env[key];
+};
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: getEnv('VITE_apiKey'),
+  authDomain: getEnv('VITE_authDomain'),
+  projectId: getEnv('VITE_projectId'),
+  storageBucket: getEnv('VITE_storageBucket'),
+  messagingSenderId: getEnv('VITE_messagingSenderId'),
+  appId: getEnv('VITE_appId'),
+  measurementId: getEnv('VITE_measurementId'),
 };
 
 const app = initializeApp(firebaseConfig);
