f167befceaca6316fb3832c249adb5400619e558
momo1108 <momo1108@daum.net>
2025-02-11 20:12:12 +0900

    chore: 모듈 최적화 관련 설정 - rollup-plugin-visualizer 설치 - vite 설정에 manualChunks 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { visualizer } from 'rollup-plugin-visualizer';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/firestore',
+            'firebase/storage',
+          ],
+        },
+      },
+    },
+  },
+  plugins: [react(), visualizer() as PluginOption],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
